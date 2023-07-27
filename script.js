6479864165a7613f9c08c41faf04cbf99aef0edc
Dany AUGUSTIN <augustindany@hotmail.com>
2023-07-27 19:31:33 -0400

    Local Storage Module added

--- script.js
+++ script.js
@@ -1,55 +1,25 @@
-const toogleButton = document.getElementsByClassName("toogle-button")[0];
-const overLay = document.getElementsByClassName("overlay")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const namef = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
 
-// eslint-disable-next-line no-unused-vars
+function storeValues() {
+	const obj = {
+		name: namef.value,
+		email: email.value,
+		message: message.value,
+	};
+	window.localStorage.setItem("myObject", JSON.stringify(obj));
+}
 
-overLay.classList.toggle("active");
-navbarLinks.classList.toggle("active");
+namef.addEventListener("input", storeValues);
+email.addEventListener("input", storeValues);
+message.addEventListener("input", storeValues);
 
-toogleButton.addEventListener("click", () => {
-	overLay.classList.toggle("active");
-	navbarLinks.classList.toggle("active");
-	document.body.classList.toggle("fixed-screen");
+window.addEventListener("load", () => {
+	const myObject = localStorage.getItem("myObject");
+	const newObj = JSON.parse(myObject);
+
+	namef.value = newObj.name || "";
+	email.value = newObj.email || "";
+	message.value = newObj.message || "";
 });
-
-const closeValue = () => {
-	navbarLinks.classList.remove("active");
-	document.body.classList.remove("fixed-screen");
-	overLay.classList.remove("active");
-};
-
-const item = (i, content, link) => {
-	let x = `a${i}`;
-	x = document.createElement("a");
-	x.setAttribute("href", `#${link}`);
-	x.textContent = content;
-	x.addEventListener("click", () => {
-		closeValue();
-	});
-
-	return x;
-};
-
-const ul = document.createElement("ul");
-const li0 = document.createElement("li");
-const li1 = document.createElement("li");
-const li2 = document.createElement("li");
-
-li0.appendChild(item(0, "Portfolio", "work-preview"));
-li1.appendChild(item(1, "About", "about"));
-li2.appendChild(item(2, "Contact", "contact"));
-ul.appendChild(li0);
-ul.appendChild(li1);
-ul.appendChild(li2);
-overLay.innerHTML = "";
-overLay.appendChild(ul);
-const cls = document.createElement("img");
-cls.classList.add("closeMenu");
-cls.setAttribute("src", "img/close.svg");
-cls.setAttribute("alt", "close");
-cls.addEventListener("click", () => {
-	closeValue();
-});
-
-overLay.appendChild(cls);
